d14096db9cbbfbe327b58140eda611b866f943f2
Kishankumary <Kishankumary@ICS-LAP-2281>
2023-01-09 18:37:34 +0530

    Add project files.

---
+++ Week6Test/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Week6Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEmployeeName = new System.Windows.Forms.Label();
+            this.lblEmployeeNo = new System.Windows.Forms.Label();
+            this.lblSalary = new System.Windows.Forms.Label();
+            this.TxtEmployeeNo = new System.Windows.Forms.TextBox();
+            this.TxtEmployeeName = new System.Windows.Forms.TextBox();
+            this.TxtSalary = new System.Windows.Forms.TextBox();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblEmployeeName
+            // 
+            this.lblEmployeeName.AutoSize = true;
+            this.lblEmployeeName.Location = new System.Drawing.Point(100, 100);
+            this.lblEmployeeName.Name = "lblEmployeeName";
+            this.lblEmployeeName.Size = new System.Drawing.Size(84, 13);
+            this.lblEmployeeName.TabIndex = 0;
+            this.lblEmployeeName.Text = "Employee Name";
+            // 
+            // lblEmployeeNo
+            // 
+            this.lblEmployeeNo.AutoSize = true;
+            this.lblEmployeeNo.Location = new System.Drawing.Point(106, 52);
+            this.lblEmployeeNo.Name = "lblEmployeeNo";
+            this.lblEmployeeNo.Size = new System.Drawing.Size(70, 13);
+            this.lblEmployeeNo.TabIndex = 1;
+            this.lblEmployeeNo.Text = "Employee No";
+            // 
+            // lblSalary
+            // 
+            this.lblSalary.AutoSize = true;
+            this.lblSalary.Location = new System.Drawing.Point(106, 147);
+            this.lblSalary.Name = "lblSalary";
+            this.lblSalary.Size = new System.Drawing.Size(36, 13);
+            this.lblSalary.TabIndex = 2;
+            this.lblSalary.Text = "Salary";
+            // 
+            // TxtEmployeeNo
+            // 
+            this.TxtEmployeeNo.Location = new System.Drawing.Point(225, 52);
+            this.TxtEmployeeNo.Name = "TxtEmployeeNo";
+            this.TxtEmployeeNo.Size = new System.Drawing.Size(100, 20);
+            this.TxtEmployeeNo.TabIndex = 3;
+            this.TxtEmployeeNo.TextChanged += new System.EventHandler(this.TxtEmployeeNo_TextChanged);
+            // 
+            // TxtEmployeeName
+            // 
+            this.TxtEmployeeName.Location = new System.Drawing.Point(225, 97);
+            this.TxtEmployeeName.Name = "TxtEmployeeName";
+            this.TxtEmployeeName.Size = new System.Drawing.Size(100, 20);
+            this.TxtEmployeeName.TabIndex = 4;
+            // 
+            // TxtSalary
+            // 
+            this.TxtSalary.Location = new System.Drawing.Point(225, 147);
+            this.TxtSalary.Name = "TxtSalary";
+            this.TxtSalary.Size = new System.Drawing.Size(100, 20);
+            this.TxtSalary.TabIndex = 5;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(109, 241);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNew.TabIndex = 6;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(250, 241);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(505, 93);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "EmployeeType";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 35);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(56, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Payroll";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(8, 63);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Consultant";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.TxtSalary);
+            this.Controls.Add(this.TxtEmployeeName);
+            this.Controls.Add(this.TxtEmployeeNo);
+            this.Controls.Add(this.lblSalary);
+            this.Controls.Add(this.lblEmployeeNo);
+            this.Controls.Add(this.lblEmployeeName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEmployeeName;
+        private System.Windows.Forms.Label lblEmployeeNo;
+        private System.Windows.Forms.Label lblSalary;
+        private System.Windows.Forms.TextBox TxtEmployeeNo;
+        private System.Windows.Forms.TextBox TxtEmployeeName;
+        private System.Windows.Forms.TextBox TxtSalary;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ Week6Test/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Week6Test
+{
+    
+    public partial class Form1 : Form
+    {
+        private SqlConnection conobj;
+        private SqlCommand cmdobj;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            private void button1_Click(object sender, EventArgs e)
+            {
+                using (Conobj = new SqlConnection(ConfigurationManager.ConnectionStrings["mastercon"].ConnectionString))
+                {
+                    using (adaobj = new SqlDataAdapter("Select * from Employee", Conobj))
+                    {
+                        SqlCommandBuilder builder = new SqlCommandBuilder(adaobj);
+                        ds = new DataSet();
+                        adaobj.Fill(ds, "Employee");
+
+
+
+                        DataRow dr = ds.Tables["Employee"].NewRow();
+                        dr["EmployeeNo"] = TxtEmployeeNo.Text;
+                        dr["EmployeeName"] = TxtEmployeeName.Text;
+                        dr["EmployeeSalary"] = TxtEmployeeSalary.Text;
+                        dr["EmployeeType"] = grpEmployeeType.Text;
+                        if (radiopayroll.Checked)
+                        {
+                            dr["EmployeeType"] = 'p';
+                        }
+                        else
+                        {
+                            dr["EmployeeType"] = 'c';
+                        }
+                    }
+    }
+}
